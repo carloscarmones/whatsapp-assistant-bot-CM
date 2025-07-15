e14db22d44bd7c514f89d088264d696f21c87677
carloscarmones <carloshenriquebal@gmail.com>
2025-07-15 08:46:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# main.py
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return return "Bot atualizado — online com Flask!"
+@app.route('/', methods=['GET'])
+def verify():
+    verify_token = "seu_token_aqui"  # <<< SUBSTITUIR pelo token que você definir na Meta
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.verify_token") == verify_token:
+        return request.args.get("hub.challenge")
+    return "Erro de verificação", 403
 
+@app.route('/', methods=['POST'])
+def webhook():
+    print(request.json)  # Teste: só mostra o corpo da mensagem recebida no log do Render
+    return "Recebido", 200
